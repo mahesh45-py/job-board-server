802d0a69193e05fca3435a1c65ed445a6c78df83
Mahesh <srikakulammahesh2085@gmail.com>
2024-08-10 21:17:07 +0530

    server.ts change

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,7 @@ connectToDatabase(ATLAS_URI).then(()=>{
     app.listen(5200,()=>{
         console.log("Server started running on http://localhost:5200")
     })
+    module.exports = app;
 }).catch((err)=>{
     console.error(err)
 })
